aa563284b92d45dc41e952cbc461afa89b921025
tt <1259747604@qq.com>
2020-03-03 14:59:32 +0800

    vue初始化

---
+++ vuepro/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      显示:
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      let res = await this.$axios('http://127.0.0.1:3000/test').then(
+        res => res.data
+      );
+      this.text = res.text;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
